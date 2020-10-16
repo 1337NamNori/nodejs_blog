bebb3ef6e642cc6828ca60cf29a436bd9a74b9b5
1337NamNori <nam20051999@gmail.com>
2020-10-16 17:32:16 +0700

    delete public/css

--- src/index.js
+++ src/index.js
@@ -1,16 +1,27 @@
 const express = require('express')
+const path = require('path')
 const morgan = require('morgan')
 const exphbs  = require('express-handlebars');
 const app = express()
 const port = 3000
+
+app.use(express.static(path.join(__dirname, 'public')))
+
 //HTTP logger
 app.use(morgan('combined'))
 //Template Engine
-app.engine('handlebars', exphbs());
-app.set('view engine', 'handlebars');
+app.engine('hbs', exphbs({
+  extname: '.hbs'
+}));
+app.set('view engine', 'hbs');
+app.set('views', path.join(__dirname,'resource/views'))
 
+//console.log(path.join(__dirname,'resource\\views'))
 app.get('/', (req, res) => {
   res.render('home')
+})
+app.get('/news', (req, res) => {
+  res.render('news')
 })
 
 app.listen(port, () => {
