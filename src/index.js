4481ba99c23d532d94ee155cacb1b5f85692bdd4
1337NamNori <nam20051999@gmail.com>
2020-10-18 03:46:20 +0700

    add prettier lint-staged husky

--- src/index.js
+++ src/index.js
@@ -1,32 +1,35 @@
-const express = require('express')
-const path = require('path')
-const morgan = require('morgan')
+const express = require('express');
+const path = require('path');
+const morgan = require('morgan');
 const exphbs = require('express-handlebars');
-const app = express()
-const port = 3000
+const app = express();
+const port = 3000;
 
-const route = require('./routes')
+const route = require('./routes');
 
-app.use(express.static(path.join(__dirname, 'public')))
-app.use(express.urlencoded({
-  extended: true
-}))
-app.use(express.json())
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(
+    express.urlencoded({
+        extended: true,
+    }),
+);
+app.use(express.json());
 
 //HTTP logger
-app.use(morgan('combined'))
+app.use(morgan('combined'));
 //Template Engine
-app.engine('hbs', exphbs({
-  extname: '.hbs'
-}));
+app.engine(
+    'hbs',
+    exphbs({
+        extname: '.hbs',
+    }),
+);
 app.set('view engine', 'hbs');
-app.set('views', path.join(__dirname, 'resource/views'))
+app.set('views', path.join(__dirname, 'resource/views'));
 
 //Routes init
-route(app)
-
-
+route(app);
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
-})
+    console.log(`Example app listening at http://localhost:${port}`);
+});
