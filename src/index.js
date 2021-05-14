9cf25f39505543e34e54c1aab1193fa30f35e514
1337NamNori <nam20051999@gmail.com>
2021-05-14 18:55:58 +0700

    Install Handelbars

---
+++ src/index.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const path = require('path');
+const morgan = require('morgan');
+const exphbs = require('express-handlebars');
+
+
+const app = express();
+const port = 3000;
+
+// HTTP Logger
+app.use(morgan('combined'));
+
+// Template Engine
+app.engine('hbs', exphbs({
+    extname: '.hbs'
+}));
+app.set('view engine', 'hbs');
+app.set('views', path.join(__dirname, '/resources/views'));
+
+// Routes
+app.get('/', (req, res) => {
+    res.render('home');
+});
+
+// Listen port 3000
+app.listen(port, () => console.log('listening on port ' + port));
