ee2e7d2192378912856cd012555ea2e15d24e271
1337NamNori <nam20051999@gmail.com>
2021-05-15 16:10:24 +0700

    Read from DB

--- src/app/controllers/homeController.js
+++ src/app/controllers/homeController.js
@@ -1,6 +1,16 @@
+const Course = require('../models/Course.js');
+const { multipleMongooseToObject } = require('../../util/mongoose.js');
+
 class HomeController {
-    index(req, res) {
-        res.render('home');
+    index(req, res, next) {
+        Course.find({})
+            .then((courses) =>
+                res.render('home', {
+                    title: 'Trang Chủ',
+                    courses: multipleMongooseToObject(courses),
+                }),
+            )
+            .catch(next);
     }
 }
 
